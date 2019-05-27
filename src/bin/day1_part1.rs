bf1bb2a57aebdc1e187e9d0fd474bbbc250ffe0b
Jeremy Miller <jgmiller88@gmail.com>
2019-05-27 08:27:50 -0700

    Add day1 part1 code

--- src/bin/day1_part1.rs
+++ src/bin/day1_part1.rs
@@ -1,3 +1,23 @@
+// Problem: https://adventofcode.com/2018/day/1
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("data/day1.txt").expect("Error opening file");
+    let reader = BufReader::new(f);
+    let mut frequency = 0;
+    for line in reader.lines() {
+        update_frequency(line.unwrap(), &mut frequency);
+    }
+    println!("Resulting Frequency: {}", frequency);
+}
+
+fn update_frequency(line: String, frequency: &mut i32) {
+    let new_frequency = line[1..].parse::<i32>().unwrap();
+    if line.starts_with('+') {
+        *frequency += new_frequency;
+    } else {
+        *frequency -= new_frequency;
+    }
 }
