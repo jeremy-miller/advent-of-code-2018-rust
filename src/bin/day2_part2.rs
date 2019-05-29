d5c1792da59f36e869db6b3e50339d1b822d6ef6
Jeremy Miller <jgmiller88@gmail.com>
2019-05-28 20:16:22 -0700

    Add day 2 part 2 code

---
+++ src/bin/day2_part2.rs
@@ -0,0 +1,21 @@
+// Problem: https://adventofcode.com/2018/day/2#part2
+
+use std::fs;
+
+#[macro_use]
+extern crate itertools;
+fn main() {
+    let contents = fs::read_to_string("data/day2.txt").expect("Error reading file");
+    for (box_id_1, box_id_2) in iproduct!(contents.lines(), contents.lines()) {
+        let (common_chars, _): (Vec<char>, Vec<char>) = box_id_1
+            .chars()
+            .zip(box_id_2.chars())
+            .filter(|(c1, c2)| c1 == c2)
+            .unzip();
+        let common_chars: String = common_chars.into_iter().collect();
+        if common_chars.len() == box_id_1.len() - 1 {
+            println!("Common Letters: {}", common_chars);
+            break;
+        }
+    }
+}
